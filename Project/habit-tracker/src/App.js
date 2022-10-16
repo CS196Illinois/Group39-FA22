d315020400dbe67ce62c070ed60d2314b0ca1d7a
534N00 <seancraggles@gmail.com>
2022-10-16 11:13:42 -0500

    Commit before 10/16/2022 Meeting

--- Project/habit-tracker/src/App.js
+++ Project/habit-tracker/src/App.js
@@ -1,4 +1,5 @@
 import Header from './components/Header'
+import Vector from './components/Vector';
 
 function App() {
   // Idk how to use this yet
@@ -10,18 +11,25 @@ function App() {
 
 
 
-
-
-
-
-
-
-
-
   return (
+
+
+
+
+
+    
     <div className="Container">
-      <Header title={'Habit Tracker'}/>
-      <h2>Welcome {'Savior'}</h2> 
+        <div> {/* vector stuff */}
+          <Vector id='V2' file={'Project\habit-tracker\public\Vector 2.png'} />
+          <Vector id='V3' file={'Project\habit-tracker\public\Vector 3.png'} />
+          <Vector id='V4' file={'Project\habit-tracker\public\Vector 4.png'} />
+          <Vector id='V5' file={'Project\habit-tracker\public\Vector 5.png'} />
+          <Vector id='V6' file={'Project\habit-tracker\public\Vector 6.png'} />
+        </div>
+
+
+        <Header title={'Habit Tracker'} />
+        <h2>Welcome {'Savior'}</h2> {/* should make into component */}
     </div>
   );
 }
---
+++ Project/habit-tracker/src/components/Vector.js
@@ -0,0 +1,11 @@
+import PropTypes from 'prop-types'
+
+const Vector = ({ file }) => {
+    return <img src="" />
+  }
+
+Vector.propTypes = {
+    file: PropTypes.string,
+}
+
+export default Vector  // not sure if this should go here but I put it in for the header (from a tutorial)
