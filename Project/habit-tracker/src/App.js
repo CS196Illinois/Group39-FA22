469d18db032688f441c0c2e22e86b7dc4d3f9ecb
534N00 <seancraggles@gmail.com>
2022-10-23 01:18:17 -0500

    Sprint 4 (Component + CSS Stuf)
    Worked on fixing up components to get image vectors to show as well as get CSS to apply to it.

--- Project/habit-tracker/src/App.js
+++ Project/habit-tracker/src/App.js
@@ -1,5 +1,11 @@
 import Header from './components/Header'
 import Vector from './components/Vector';
+import v2 from './imgs/Vector 2.png';
+import v3 from './imgs/Vector 3.png';
+import v4 from './imgs/Vector 4.png';
+import v5 from './imgs/Vector 5.png';
+import v6 from './imgs/Vector 6.png';
+import './index.css'
 
 function App() {
   // Idk how to use this yet
@@ -20,16 +26,16 @@ function App() {
     
     <div className="Container">
         <div> {/* vector stuff */}
-          <Vector id='V2' file={'Project\habit-tracker\public\Vector 2.png'} />
-          <Vector id='V3' file={'Project\habit-tracker\public\Vector 3.png'} />
-          <Vector id='V4' file={'Project\habit-tracker\public\Vector 4.png'} />
-          <Vector id='V5' file={'Project\habit-tracker\public\Vector 5.png'} />
-          <Vector id='V6' file={'Project\habit-tracker\public\Vector 6.png'} />
+          <Vector id='V2' file={v2} alt={'v2'} />
+          <Vector id='V3' file={v3} alt={'v3'} />
+          <Vector id='V4' file={v4} alt={'v4'} />
+          <Vector id='V5' file={v5} alt={'v5'}/>
+          <Vector id='V6' file={v6} alt={'v6'} />
         </div>
 
 
         <Header title={'Habit Tracker'} />
-        <h2>Welcome {'Savior'}</h2> {/* should make into component */}
+        <h2>Welcome {'Developer'}</h2> {/* should make into component */}
     </div>
   );
 }
--- Project/habit-tracker/src/components/Header.js
+++ Project/habit-tracker/src/components/Header.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import PropTypes from 'prop-types'
 
 // Return header of title name
