635d9e6f47d3346b5bc5b5d7152e3b096fb375a0
534N00 <seancraggles@gmail.com>
2022-11-05 17:45:52 -0500

    Sprint 6 Work
    Fixed up the CSS on the page and got the dimensions about right + figured out how get the Inter font family imported.

--- Project/habit-tracker/src/App.js
+++ Project/habit-tracker/src/App.js
@@ -1,42 +1,34 @@
-import Header from './components/Header'
+import Header from './components/Header';
+import HeaderTwo from './components/HeaderTwo.js';
 import Vector from './components/Vector';
 import v2 from './imgs/Vector 2.png';
 import v3 from './imgs/Vector 3.png';
 import v4 from './imgs/Vector 4.png';
-import v5 from './imgs/Vector 5.png';
-import v6 from './imgs/Vector 6.png';
-import './index.css'
+import v5 from './imgs/Vector 5 (6).png'; // Vector 6 in the Figma design
+import './index.css';
 
 function App() {
   // Idk how to use this yet
   const user = (str = 'Guest') => {
-    return user;
+    return str;
   }
-
-
-
-
 
   return (
 
-
-
-
-
-    
     <div className="Container">
-        <div> {/* vector stuff */}
-          <Vector id='V2' file={v2} alt={'v2'} />
-          <Vector id='V3' file={v3} alt={'v3'} />
-          <Vector id='V4' file={v4} alt={'v4'} />
-          <Vector id='V5' file={v5} alt={'v5'}/>
-          <Vector id='V6' file={v6} alt={'v6'} />
-        </div>
-
-
+      <div> {/* vector stuff */}
+        <Vector id='V2' file={v2} alt={'v2'} />
+        <Vector id='V3' file={v3} alt={'v3'} />
+        <Vector id='V3-2' file={v3} alt={'v3-2'} />
+        <Vector id='V4' file={v4} alt={'v4'} />
+        <Vector id='V5' file={v5} alt={'v5'}/>
+      </div>
+      <div>
         <Header title={'Habit Tracker'} />
-        <h2>Welcome {'Developer'}</h2> {/* should make into component */}
+        <HeaderTwo text={'Welcome'} name={user('Developer')} /> {/* IDK what I'm doing with this component yet */}
+      </div>
     </div>
+
   );
 }
 
--- Project/habit-tracker/src/components/Vector.js
+++ Project/habit-tracker/src/components/Vector.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
+import PropTypes from 'prop-types';
 
 const Vector = ({ id, file, alt }) => {
     return <img id={id} src={file} alt={alt}/>
@@ -10,4 +10,4 @@ Vector.propTypes = {
     alt: PropTypes
 }
 
-export default Vector  // not sure if this should go here but I put it in for the header (from a tutorial)
+export default Vector;  // not sure if this should go here but I put it in for the header (from a tutorial)
