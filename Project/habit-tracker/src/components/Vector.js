1375b4ab996d3677dfc66620a8c39ea2a3362a04
nzhan2 <nzhan2@illinois.edu>
2022-12-02 12:12:23 -0600

    Combined Sean's code with first box

--- Project/habit-tracker/src/components/Vector.js
+++ Project/habit-tracker/src/components/Vector.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
 
-const Vector = ({ file }) => {
-    return <img src="" />
+const Vector = ({ id, file, alt }) => {
+    return <img id={id} src={file} alt={alt}/>
   }
 
 Vector.propTypes = {
     file: PropTypes.string,
+    alt: PropTypes
 }
 
 export default Vector  // not sure if this should go here but I put it in for the header (from a tutorial)
